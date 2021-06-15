40e785bfe60c33b16f790dfe920b57312b09f4a2
Nicholas Radtke <Raddnc@gmail.com>
2021-06-15 18:24:58 -0500

    setting up portfolio

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Hello and Welcome to my react website! Ill be showing off my skills
+          and projects!
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
