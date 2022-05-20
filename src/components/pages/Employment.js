d9d4c709f9cc10c1c241ecf5207a77e58c3ac2ef
RadtkeN <raddnc@gmail.com>
2022-05-20 03:30:07 -0500

    scrapped project to rebuild

--- src/components/pages/Employment.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import '../../App.css'
-
-
-export default function Employment() {
-    return <h1 className='employment'>Employment</h1>
-}
--- src/components/pages/Resume.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import '../../App.css'
-
-
-export default function Resume() {
-    return <h1 className='resume'>Resume</h1>
-}
